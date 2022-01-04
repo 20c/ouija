4721189a76ddc306a475fa7ca5a2296bd6f1f2ed
Stefan Pratter <stefan@20c.com>
2022-01-04 12:25:36 +0000

    api-docs

---
+++ docs/api/api.js
@@ -0,0 +1,16 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "twentyc.rest.Client",
+        "twentyc.rest.Form",
+        "twentyc.rest.List",
+        "twentyc.rest.PermissionForm",
+        "twentyc.rest.Response",
+        "twentyc.rest.Select",
+        "twentyc.rest.Widget"
+    ],
+    "modules": [],
+    "allModules": [],
+    "elements": []
+} };
+});
